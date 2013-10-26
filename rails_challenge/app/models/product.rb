a645561bf96fd96b73305cd43100a9421732ce68
brett-anderson <brettanderson16@gmail.com>
2013-10-26 17:36:00 -0500

    Final commit before submission

--- rails_challenge/app/models/product.rb
+++ rails_challenge/app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :price, :stock_quantity, :title
+  validates       :description, :price, :stock_quantity, :title,
+                  presence: true
 end
